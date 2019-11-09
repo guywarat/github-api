3a067ee510dfdd958d0e57a291d7539c1ae194b5
Guy <worst.siriko@gmail.com>
2019-11-09 12:54:00 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function onInit(){
+    axios.get('https://api.github.com/users/guywarat')
+        .then(function(response){
+        console.log(response);
+        console.log(response.data.avatar_url);
+        document.getElementById('avatar').src = response.data.avatar_url;
+        document.getElementById('username').innerHTML='Guywarat';
+    });
+    
+}
